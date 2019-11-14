3eee393cb851d01034eeb2b2ce3e15f6b8b3912a
Sachin Gurung <saching061@hotmail.com>
2019-11-13 21:45:45 -0800

    First Commit

--- FactorialNumber/Program.cs
+++ FactorialNumber/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace FactorialNumber
 {
+    //Write a program and ask the user to enter a number. Compute the factorial of the number and print it on the console. 
+    //For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120.
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter any number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            var factorial = 1;
+
+            for (int i = 1; i <=number ; i++)
+            
+                factorial = factorial * i;
+                Console.WriteLine("Factorial of " + number + " is :" + factorial);           
+         
         }
     }
 }
+/*
+Enter any number: 6
+Factorial of 6 is :720
+Press any key to continue . . .
+*/
